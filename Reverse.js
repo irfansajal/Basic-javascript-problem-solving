2f18c09352eb998508232875f29fd397a38ccb37
irfansajal <irfan@Irfans-MacBook-Pro.local>
2024-04-07 23:36:26 +0200

    added

---
+++ Reverse.js
@@ -0,0 +1,35 @@
+// write two name and take them as a function parameter  and reverse and show the longest name
+
+/* const findBiggerName = (nam) => {
+  let longestName = "";
+
+  for (let i = 0; i < nam.length; i++) {
+    //console.log(nam[i]);
+    let ele = nam[i];
+    let find = ele.split("").reverse("").join("");
+
+    if (ele.length > longestName.length) {
+      longestName = find;
+    }
+  }
+  return longestName;
+};
+
+let name = ["alina", "tony"];
+let result = findBiggerName(name);
+console.log(result); */
+// second way 
+
+const reverseName = (name1,name2)=> {
+    let first = name1.length
+    let second = name2.length
+    if (first > second){
+        return name1.split('').reverse().join('')
+    }
+    else{
+        return name2.split("").reverse().join("");
+    }
+
+}
+let result = reverseName('alina','bony')
+console.log(result);
